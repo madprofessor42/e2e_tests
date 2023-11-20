97266ec46bb82647c30ec94e9c8e7561c1fffce3
madprofessor42 <129622292+madprofessor42@users.noreply.github.com>
2023-11-20 16:30:11 +0300

    Update taxi.spec.js

--- e2e/tests/taxi.spec.js
+++ e2e/tests/taxi.spec.js
@@ -1,10 +1,8 @@
 const { test, expect} = require('@playwright/test');
 const { webkit, chromium } = require('playwright');  // Or 'chromium' or 'webkit'.
-const { yangoLite } = require('../browsers');
-
 
 test.describe('Test', () => {
-    test('Test YangoLite Safari', async () => {
+    test('Test Safari', async () => {
         const browser = await webkit.launch();
         const context = await browser.newContext()
 
